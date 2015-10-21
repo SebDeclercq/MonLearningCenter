913d9e75419b1f5194bb9770824cee2c825eb155
SebDeclercq <miniseb3.0@gmail.com>
2015-10-21 20:07:38 +0200

    Ajout CrudManagement

--- PHP/GestionProduitsCulturels/ICulturalProduct.php
+++
@@ -1,5 +0,0 @@
-<?php
-interface ICulturalProduct
-{
-    public function getMetadata();
-}
--- PHP/GestionProduitsCulturels/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('Client.php');
-
-$worker = new Client;
-
-foreach ($worker->getCulturalProducts() as $product) {
-    echo $product->getMetadata()."<br/>";
-}
